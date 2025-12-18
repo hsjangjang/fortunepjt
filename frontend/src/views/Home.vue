83cd0de1ea7ebde94801ee997b53d7deb1da5ba2
lalaamy <lalaamy@naver.com>
2025-12-18 17:47:11 +0900

    메인 문구 변경2

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,7 +9,7 @@
           </span>
         </div>
         <h1 class="display-title fw-bold mb-4">
-          행운의 픽<br>당신의 하루를 열어보아요
+          행운을 PICK✨<br>당신의 하루를 열어보아요
         </h1>
         <p class="lead text-muted mb-5">
           사주와 별자리를 분석하여<br>
