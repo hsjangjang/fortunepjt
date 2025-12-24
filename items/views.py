a971580495818b174d77214216f0c675d41e0b07
hsjangjang <99gktjs2937@naver.com>
2025-12-25 02:19:12 +0900

    불필요 파일 삭제, 미사용 코드 삭제, 코드 리팩토링

--- items/views.py
+++
@@ -1,6 +0,0 @@
-# items/views.py
-# 레거시 템플릿 뷰는 모두 API로 대체되어 삭제됨
-# API 엔드포인트: items/api_views.py 참조
-
-# 이 파일은 하위 호환성을 위해 유지
-# 실제 기능은 api_views.py에서 처리
--- recommendations/views.py
+++
@@ -1,3 +0,0 @@
-# recommendations/views.py
-# 레거시 템플릿 뷰는 모두 API로 대체되어 삭제됨
-# API 엔드포인트: recommendations/api_views.py 참조
